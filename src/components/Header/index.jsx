270f543f896f23d29720aba9ac46cccd4666a08f
helder <helderks2104@hotmail.com>
2023-09-21 17:34:02 -0300

    Adicionado um header inicial apenas para base

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,54 @@
 import React from 'react';
 import './index.css';
+import { FaHome, FaInfo, FaAndroid, FaAngellist } from 'react-icons/fa';
+import { HiQrcode } from 'react-icons/hi';
+
 export default function Header() {
   return (
     <>
-      <h1 className="mano">Hello World</h1>
+      <div className="header-container">
+        <div className="logo">
+          <HiQrcode size={80} />
+        </div>
+        <ul className="ul">
+          <li className="li">
+            <a href="" className="a">
+              <i className="i">
+                <FaHome />
+              </i>
+            </a>
+          </li>
+          <li className="li">
+            <a href="" className="a">
+              <i className="i">
+                <FaInfo />
+              </i>
+            </a>
+          </li>
+          <li className="li">
+            <a href="#" className="a">
+              <i className="i">
+                <FaAndroid />
+              </i>
+            </a>
+          </li>
+          <li className="li">
+            <a href="" className="a">
+              <i className="i">
+                <FaAngellist />
+              </i>
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <h1>
+          Example heading
+          <Badge bg="secondary" as={Button}>
+            New
+          </Badge>
+        </h1>
+      </div>
     </>
   );
 }
